b371f2f932984c09311034d439941441473c12de
Huu Nhut <ngohuunhut16@gmail.com>
2020-08-25 15:19:35 +0700

    Init Project

---
+++ resources/lang/en/message.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'addtask' => 'Add New Task',
+    'addnewtask' => 'Add Task',
+    'tasklist' => 'List Task',
+    'currtask' => 'Current Tasks',
+    'task' => 'Tasks',
+    'delete' => 'Delete',
+    'nametask' => 'Input task',
+    'errortit' => 'Whoops! Something went wrong!',
+];
